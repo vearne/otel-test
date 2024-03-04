a6c31073a6ba3511c3f4f41e6911ca45ccffa3cf
vearne <asdwoshiaotian@gmail.com>
2024-03-04 20:30:24 +0800

    update micro_service/say_hello_grpc/main.go

--- micro_service/say_hello_grpc/main.go
+++ micro_service/say_hello_grpc/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
 	zlog "github.com/vearne/otel-test/log"
+	"github.com/vearne/otel-test/myotel"
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -12,6 +14,7 @@ import (
 	"log"
 	"net"
 	"sync/atomic"
+	"time"
 
 	_ "github.com/apache/skywalking-go"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
@@ -22,6 +25,15 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+func init() {
+	myotel.InitTracerProvider()
+	myotel.InitMeterProvider()
+	err := runtime.Start(runtime.WithMinimumReadMemStatsInterval(time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 var counter uint64 = 0
 
 const (
@@ -29,6 +41,40 @@ const (
 )
 
 var rdb *redis.Client
+
+func main() {
+	zlog.InitLogger("/tmp/sayHelloGrpc.log", "debug")
+
+	rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	// Enable tracing instrumentation.
+	if err := redisotel.InstrumentTracing(rdb); err != nil {
+		panic(err)
+	}
+
+	// Enable metrics instrumentation.
+	if err := redisotel.InstrumentMetrics(rdb); err != nil {
+		panic(err)
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer(
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	)
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+
+	log.Println("say_hello_grpc starting...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
 
 type server struct {
 	pb.UnimplementedGreeterServer
@@ -62,39 +108,5 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	default:
 		return &pb.HelloReply{Message: "Hello " + in.Name},
 			status.Error(codes.Unauthenticated, "--Unauthenticated--")
-	}
-}
-
-func main() {
-	zlog.InitLogger("/tmp/sayHelloGrpc.log", "debug")
-
-	rdb = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-
-	// Enable tracing instrumentation.
-	if err := redisotel.InstrumentTracing(rdb); err != nil {
-		panic(err)
-	}
-
-	// Enable metrics instrumentation.
-	if err := redisotel.InstrumentMetrics(rdb); err != nil {
-		panic(err)
-	}
-
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer(
-		grpc.StatsHandler(otelgrpc.NewServerHandler()),
-	)
-	pb.RegisterGreeterServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-
-	log.Println("say_hello_grpc starting...")
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
 	}
 }
