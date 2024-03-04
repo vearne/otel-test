b6b8425efc625a7197f97429976845ff638924a9
vearne <asdwoshiaotian@gmail.com>
2024-03-04 18:53:34 +0800

    init project

---
+++ log/log.go
@@ -0,0 +1,95 @@
+package log
+
+import (
+	"context"
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var (
+	DefaultLogger *otelzap.Logger
+)
+
+func InitLogger(logPath string, level string) {
+	alevel := zap.NewAtomicLevel()
+
+	hook := lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    1024, // megabytes
+		MaxBackups: 10,
+		MaxAge:     7,    //days
+		Compress:   true, // disabled by default
+	}
+	w := zapcore.AddSync(&hook)
+
+	switch level {
+	case "debug":
+		alevel.SetLevel(zap.DebugLevel)
+	case "info":
+		alevel.SetLevel(zap.InfoLevel)
+	case "error":
+		alevel.SetLevel(zap.ErrorLevel)
+	default:
+		alevel.SetLevel(zap.InfoLevel)
+	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05")
+	encoderConfig.ConsoleSeparator = " | "
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		w,
+		alevel,
+	)
+
+	logger := zap.New(core)
+	logger = logger.WithOptions(zap.AddCaller(), zap.AddCallerSkip(1))
+
+	DefaultLogger = otelzap.New(logger,
+		otelzap.WithTraceIDField(true),
+		otelzap.WithMinLevel(zap.DebugLevel),
+	)
+}
+
+func DebugContext(ctx context.Context, msg string, fields ...zapcore.Field) {
+	DefaultLogger.DebugContext(ctx, msg, fields...)
+}
+
+func InfoContext(ctx context.Context, msg string, fields ...zapcore.Field) {
+	DefaultLogger.InfoContext(ctx, msg, fields...)
+}
+
+func WarnContext(ctx context.Context, msg string, fields ...zapcore.Field) {
+	DefaultLogger.WarnContext(ctx, msg, fields...)
+}
+
+func ErrorContext(ctx context.Context, msg string, fields ...zapcore.Field) {
+	DefaultLogger.ErrorContext(ctx, msg, fields...)
+}
+
+func FatalContext(ctx context.Context, msg string, fields ...zapcore.Field) {
+	DefaultLogger.FatalContext(ctx, msg, fields...)
+}
+
+func Debug(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Debug(msg, fields...)
+}
+
+func Info(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zapcore.Field) {
+	DefaultLogger.Fatal(msg, fields...)
+}
---
+++ myotel/otel.go
@@ -0,0 +1,68 @@
+package myotel
+
+import (
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+
+	//"go.opentelemetry.io/otel/propagation"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	sdkresource "go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"log"
+	"sync"
+)
+
+var resource *sdkresource.Resource
+var initResourcesOnce sync.Once
+
+func InitTracerProvider() *sdktrace.TracerProvider {
+	ctx := context.Background()
+
+	exporter, err := otlptracegrpc.New(ctx)
+	if err != nil {
+		log.Fatalf("new otlp trace grpc exporter failed: %v", err)
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(initResource()),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
+
+func InitMeterProvider() *sdkmetric.MeterProvider {
+	ctx := context.Background()
+
+	exporter, err := otlpmetricgrpc.New(ctx)
+	if err != nil {
+		log.Fatalf("new otlp metric grpc exporter failed: %v", err)
+	}
+
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exporter)),
+		sdkmetric.WithResource(initResource()),
+	)
+	otel.SetMeterProvider(mp)
+	return mp
+}
+
+func initResource() *sdkresource.Resource {
+	initResourcesOnce.Do(func() {
+		extraResources, _ := sdkresource.New(
+			context.Background(),
+			sdkresource.WithOS(),
+			sdkresource.WithProcess(),
+			sdkresource.WithContainer(),
+			sdkresource.WithHost(),
+		)
+		resource, _ = sdkresource.Merge(
+			sdkresource.Default(),
+			extraResources,
+		)
+	})
+	return resource
+}
